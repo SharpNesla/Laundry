a2be95ac8771bedc8564a94a90c5c853eba6833f
Unknown <stylerkov@yandex.ru>
2019-04-28 02:11:02 +0800

    Washer dashboard implementation
    Washer dashboard implementation

--- Laundry/Views/Dashboards/WasherDashBoard.xaml.cs
+++ Laundry/Views/Dashboards/WasherDashBoard.xaml.cs
@@ -14,13 +14,41 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Caliburn.Micro;
 using Laundry.Model;
+using Laundry.Utils;
+using Laundry.Utils.Controls;
+using Laundry.Views.Actions;
 
 namespace Laundry.Views.Dashboards
 {
   public class WasherDashBoardViewModel : DashBoardBase
   {
-    public WasherDashBoardViewModel(IEventAggregator aggregator, IModel model) : base(aggregator, model)
+    private readonly OrderDataGridViewModel _actionsOrderGrid;
+    public OrderDataGridViewModel OrderGrid { get; internal set; }
+
+    public PaginatorViewModel Paginator { get; internal set; }
+
+    public bool IsWashOrders { get; set; }
+    public bool IsApplyOrdersForDelivery { get; set; }
+    public bool IsAcceptDelivery { get; set; }
+
+    public WasherDashBoardViewModel(IEventAggregator aggregator, IModel model, PaginatorViewModel paginator,
+      OrderDataGridViewModel orderGrid, OrderDataGridViewModel actionsOrderGrid) : base(
+      aggregator, model)
     {
+      _actionsOrderGrid = actionsOrderGrid;
+      IsWashOrders = true;
+
+      this.Paginator = paginator;
+      this.Paginator.RegisterPaginable(orderGrid);
+      this.Paginator.ElementsName = "Заказов";
+
+      this.OrderGrid = orderGrid;
+    }
+
+    public async void WashOrders()
+    {
+      var wash = new WashOrdersViewModel(Model, _actionsOrderGrid);
+      await DialogHostExtensions.ShowCaliburnVM(wash);
     }
   }
 }
